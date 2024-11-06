7933d6fe4434760343b4c558ebd6be5fafa1af85
mainoloYawe <220712047@students.uajy.ac.id>
2024-11-06 21:49:56 +0700

    Update Tugas_Modul4_B_SIMSON_12047.py

--- Tugas_Modul4_B_SIMSON_12047.py
+++ Tugas_Modul4_B_SIMSON_12047.py
@@ -57,12 +57,10 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - SIMSON</h1>", unsafe_allow_html=True) 
     st.dataframe(input_data)
 
-    model_directory = r'D:\Informatika\semester 5\Pembelajaran Mesin dan Pembelajaran Mendalam B\Unsupervised Learning (Praktek)\Unsupervised Learning (Praktek)\Tugas4_B_12047'
-
     model_path = {
-        "AGG_model": os.path.join(model_directory, r'AGG_model.pkl'),
-        "KMeans_model": os.path.join(model_directory, r'KMeans_model.pkl'),
-        "DBSCAN_model": os.path.join(model_directory, r'DBSCAN_model.pkl'),
+        "AGG_model": 'AGG_model.pkl',
+        "KMeans_model": r'KMeans_model.pkl'),
+        "DBSCAN_model": r'DBSCAN_model.pkl'),
     }
 
     models = {}
